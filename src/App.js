007e0f0c8ec46b22436cccc388d0cb02e0612e48
debashis <debashisbaral469@gmail.com>
2024-08-23 07:24:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Header from './components/Header/Header'
+import Grid from '@mui/material/Grid'
+import Map from './components/Map/Map'
+import List from './components/List/List'
 
-function App() {
+import   GetPlacesData  from './api/GetPlacesData'
+
+const App = () => {
+  const [places, setPlaces] = useState([]) ;
+  const [coordinates, setCoordinates] = useState({}) ;
+  const [bounds, setBounds] = useState(null) ;
+  const [childClicked, setChildClicked] = useState(null) ;
+
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const [isLoading, setLoading] = useState(false) ;
+
+  useEffect(() => {
+  
+     navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+       setCoordinates( {lat: latitude, lng: longitude} ) ;
+     })
+  },[])
+
+  useEffect(() => {
+    // console.log(bounds) ;
+    setLoading(true) ;
+    if(bounds ) {
+
+      GetPlacesData(type,bounds.ne, bounds.sw)
+      .then ((data) => {
+         console.log("Restaurants : " , data) ;
+         setPlaces(data) ;
+         setLoading(false) ;
+      } )
+
+      
+    }
+    
+  }, [ bounds, coordinates, type ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Header />
+    <Grid container spacing={3} sx={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List 
+            places = {places}  
+            childClicked = {childClicked} 
+            isLoading={isLoading}
+            type={type} 
+            setType = {setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid> 
+        <Grid item sx={{height:'90vh'}} xs={12} md={8}>
+          <Map 
+            setCoordinates = {setCoordinates}
+            setBounds = {setBounds} 
+            coordinates = {coordinates}
+            places = {places} 
+            setChildClicked = {setChildClicked} 
+          />
+        </Grid>      
+    </Grid>
+    </>
+  )
 }
 
-export default App;
+export default App
